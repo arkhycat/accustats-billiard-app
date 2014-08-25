deb7e3a585d5e5d16bf71ecc9b02a231b18f1a7c
arkhycat <chehirscat@gmail.com>
2014-08-25 12:01:24 +0400

    turn object working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,13 +15,18 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.bootstrap'
   ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,16 +9,40 @@
  */
 angular.module('accustatsBilliardAppApp')
   .controller('MainCtrl', function ($scope, $window) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
     $scope.windowHeight = $window.innerHeight;
-    $scope.player1 = "player1";
-    $scope.player2 = "player2";
-    $scope.tabClicked = function () 
-    {
-    	
+    $scope.players = ["player1", "player2"];
+    $scope.turnTypes = ["KICK", "MISS", "SAFETY"];
+    $scope.turns = [];
+    var CreateTurn = function () {
+      this.player = 0;
+      this.turnType = 0;
+      this.count = 0;
+      this.setPlayer = function (index) {
+          this.player = index;
+      };
+
+      this.setType = function (type) {
+        this.turnType = type;
+      };
+
+    };
+
+    $scope.turn = new CreateTurn();
+    $scope.nextPlayer = function () {
+      return ($scope.turn.player + 1) % $scope.players.length;  
+    };
+
+    $scope.setPlayer = function (index) {$scope.turn.setPlayer(index)};
+    $scope.setType = function (index) {$scope.turn.setType(index)};
+
+    $scope.getPlayer = function (index) {return $scope.turn.player};
+    $scope.getType = function (index) {return $scope.turn.turnType};
+
+    $scope.addTurn = function () {
+      $scope.turns.push($scope.turn);
+      var nextPlayer = $scope.nextPlayer();
+      $scope.turn = new CreateTurn();
+      $scope.turn.setPlayer(nextPlayer);
     };
   });
